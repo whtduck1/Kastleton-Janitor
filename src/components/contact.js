9450a0265982678df17ed4ba8a908eb3b8130e93
whtduck1 <myszormateusz02@gmail.com>
2025-01-11 21:16:47 +0100

    header

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function contact() {
+function Contact() {
   return (
     <div>
       <h1>Contact</h1>
@@ -9,4 +9,4 @@ function contact() {
   );
 }
 
-export default contact;
+export default Contact;
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,38 @@
+import React, { useState } from 'react';
+import '../styles/header.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 function Header() {
-    return (
-      <header>
-        <h1>Header</h1>
-        <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#pricing">Pricing</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log('Menu state:', isMenuOpen); // Debugowanie
+  };
+
+  return (
+    <header>
+      <div className="header-container">
+        <div className="title-container">
+          <h1>Kastleton Janitor</h1>
+          <p>your guild discord organizer</p>
+        </div>
+        <div className="navigation-container">
+          <button className="menu-toggle" onClick={toggleMenu}>
+            {isMenuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+          <nav className={isMenuOpen ? 'open' : ''}>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#pricing">Pricing</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
