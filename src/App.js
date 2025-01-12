de1d44a4468e720b2cf33e98ca9707028ddf4a4f
whtduck1 <myszormateusz02@gmail.com>
2025-01-12 13:16:50 +0100

    animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { motion } from 'framer-motion';
 import Header from './components/header';
 import Footer from './components/footer';
 import Home from './components/home';
@@ -7,26 +8,55 @@ import About from './components/about';
 import Pricing from './components/pricing';
 import Contact from './components/contact';
 
+const animationVariants = {
+  hidden: { opacity: 0, y: -60 },
+  visible: { opacity: 1, y: 0, transition: { duration: 2 } },
+};
+
 function App() {
-    return(
+  return (
     <>
-    <Header />
-    <main>
-      <section id="home">
-        <Home />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="pricing">
-        <Pricing />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-    </main>
-    <Footer />
-  </>
+      <Header />
+      <main>
+        <motion.section
+          id="home"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={animationVariants}
+        >
+          <Home />
+        </motion.section>
+        <motion.section
+          id="about"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={animationVariants}
+        >
+          <About />
+        </motion.section>
+        <motion.section
+          id="pricing"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={animationVariants}
+        >
+          <Pricing />
+        </motion.section>
+        <motion.section
+          id="contact"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={animationVariants}
+        >
+          <Contact />
+        </motion.section>
+      </main>
+      <Footer />
+    </>
   );
 }
 
